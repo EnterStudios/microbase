98bd87663d71d8737b584902d891dd06c7d79446
Nicolás Cornaglia <ncornag@gmail.com>
2017-01-24 22:22:51 +0100

    Updated examples.

--- examples/micro-math-service/src/index.js
+++ examples/micro-math-service/src/index.js
@@ -1,6 +1,8 @@
-const base = require('microbase')();
+const base = require('microbase')({
+  configFiles: [
+    `${__dirname}/config/${process.env.NODE_ENV || 'development'}.json`,
+    `${__dirname}/config/defaults.json`
+  ]
+});
 
-// Add operations
-base.services.addOperationsFromFolder('operations', base);
-
-module.exports = base;
+base.services.addOperationsFromFolder(`${__dirname}/operations`);
